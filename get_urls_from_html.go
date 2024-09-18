dbc2bdc1112c0e6e72210a4f862f3000a1f15b07
bmeek0110 <brandon.meek.q2.com>
2024-09-17 23:21:30 -0500

    Finished woo

--- get_urls_from_html.go
+++ get_urls_from_html.go
@@ -1,131 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"net/url"
-	"reflect"
 	"strings"
-	"testing"
+
+	"golang.org/x/net/html"
 )
 
-func TestGetURLsFromHTML(t *testing.T) {
-	cases := []struct {
-		name          string
-		inputURL      string
-		inputBody     string
-		expected      []string
-		errorContains string
-	}{
-		{
-			name:     "absolute URL",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-<html>
-	<body>
-		<a href="https://blog.boot.dev">
-			<span>Boot.dev</span>
-		</a>
-	</body>
-</html>
-`,
-			expected: []string{"https://blog.boot.dev"},
-		},
-		{
-			name:     "relative URL",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-<html>
-	<body>
-		<a href="/path/one">
-			<span>Boot.dev</span>
-		</a>
-	</body>
-</html>
-`,
-			expected: []string{"https://blog.boot.dev/path/one"},
-		},
-		{
-			name:     "absolute and relative URLs",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-<html>
-	<body>
-		<a href="/path/one">
-			<span>Boot.dev</span>
-		</a>
-		<a href="https://other.com/path/one">
-			<span>Boot.dev</span>
-		</a>
-	</body>
-</html>
-`,
-			expected: []string{"https://blog.boot.dev/path/one", "https://other.com/path/one"},
-		},
-		{
-			name:     "no href",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-<html>
-	<body>
-		<a>
-			<span>Boot.dev></span>
-		</a>
-	</body>
-</html>
-`,
-			expected: nil,
-		},
-		{
-			name:     "bad HTML",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-<html body>
-	<a href="path/one">
-		<span>Boot.dev></span>
-	</a>
-</html body>
-`,
-			expected: []string{"https://blog.boot.dev/path/one"},
-		},
-		{
-			name:     "invalid href URL",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-<html>
-	<body>
-		<a href=":\\invalidURL">
-			<span>Boot.dev</span>
-		</a>
-	</body>
-</html>
-`,
-			expected: nil,
-		},
+func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	htmlReader := strings.NewReader(htmlBody)
+	doc, err := html.Parse(htmlReader)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse HTML: %v", err)
 	}
 
-	for i, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			baseURL, err := url.Parse(tc.inputURL)
-			if err != nil {
-				t.Errorf("Test %v - '%s' FAIL: couldn't parse input URL: %v", i, tc.name, err)
-				return
-			}
+	var urls []string
+	var traverseNodes func(*html.Node)
+	traverseNodes = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, anchor := range node.Attr {
+				if anchor.Key == "href" {
+					href, err := url.Parse(anchor.Val)
+					if err != nil {
+						fmt.Printf("couldn't parse href '%v': %v\n", anchor.Val, err)
+						continue
+					}
 
-			actual, err := getURLsFromHTML(tc.inputBody, baseURL)
-			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
-				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
-				return
-			} else if err != nil && tc.errorContains == "" {
-				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
-				return
-			} else if err == nil && tc.errorContains != "" {
-				t.Errorf("Test %v - '%s' FAIL: expected error containing '%v', got none.", i, tc.name, tc.errorContains)
-				return
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
+				}
 			}
+		}
 
-			if !reflect.DeepEqual(actual, tc.expected) {
-				t.Errorf("Test %v - '%s' FAIL: expected URLs %v, got URLs %v", i, tc.name, tc.expected, actual)
-				return
-			}
-		})
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			traverseNodes(child)
+		}
 	}
+	traverseNodes(doc)
+
+	return urls, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bmeek0110/webcrawler
 
 go 1.23.0
+
+require golang.org/x/net v0.29.0
